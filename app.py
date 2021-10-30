f402ee3f1d2feee555df8ce0944f47bd729f907d
bpratt <bpratt@us.ibm.com>
2021-10-29 22:19:45 -0400

    punctuation

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ while True:
     print(request)
 
     # Send HTTP response
-    response = 'HTTP/1.0 200 OK\n\nHello World'
+    response = 'HTTP/1.0 200 OK\n\nHello World?!'
     client_connection.sendall(response.encode())
     client_connection.close()
 
